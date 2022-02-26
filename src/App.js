3b296ed08d7f2803e1252dcdbf2714f1bdf1f633
Niya Ma <78030299+Bellatrixmny@users.noreply.github.com>
2022-02-26 12:39:37 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { List } from "./components/Pages/List";
+import Contact from "./components/Pages/Contact";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+/>
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/List" component={List} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
