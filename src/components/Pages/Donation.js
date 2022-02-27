90874dc9e0f36b6c69b438a96e13196875e83aae
Niya Ma <78030299+Bellatrixmny@users.noreply.github.com>
2022-02-27 00:28:53 -0500

    connect crowdfunding to firebase

--- src/components/Pages/Donation.js
+++ src/components/Pages/Donation.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { memo, useState, useEffect }from "react";
 import {Table, ProgressBar, tr, td, thead, InputGroup, FormControl} from 'react-bootstrap'
+import { useTable } from "react-table";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Styles.css';
+import { useSSRSafeId } from "@react-aria/ssr";
+import { useAuth0 } from '@auth0/auth0-react';
+import { doc, setDoc, collection, query, getDocs  } from "firebase/firestore";
+import db from "./firebase_config";
+import { render } from "@testing-library/react";
+
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
 
-const user = [10,20,30]
-
 export const Donation = () => {
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const getData = async (e) => {
+      const q = query(collection(db, "CrowdFundingProjects"));
+      const querySnapshot = await getDocs(q);
+  
+      await querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          items.push(doc.data())
+      });
+      console.log(items);
+    }
+    getData();
+  }, []);
+  const headers = ["Title", "Description", "Author", "Goal", "Address"];
+  const attributes = ["title", "description", "author", "goal", "address"];
 
   return (
-    
-    <div>
-      
-     <ProgressBar now={user[0]} />
-
-
-    </div>
+      <Table responsive>
+      <thead>
+        <tr>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <th key={index}>{headers[index]}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, index) => (
+          <tr>
+            {Array.from({ length: 5 }).map((_, index) => (
+            <td key={index}>{item[attributes[index]]}</td>
+            ))}
+          </tr>
+          ))}
+        </tbody>
+      </Table>
   );
+  
 };
